e8515719deca8a009254a6b5feafd5f63cdb2c5b
shivam_since2002 <sg2109964@gmail.com>
2024-03-01 18:01:11 +0530

    Container Ready Now time to set animation

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,54 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello Worldd!'),
-        ),
-      ),
-    );
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.black,
+          body: Stack(
+            children: <Widget>[
+              Positioned(
+                top: 300,
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  decoration: const BoxDecoration(color: Colors.deepPurple),
+                ),
+              ),
+              Positioned(
+                top: 300,
+                right: 0,
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  decoration: const BoxDecoration(color: Colors.deepPurple),
+                ),
+              ),
+              Positioned(
+                top: 90,
+                right: 80,
+                child: Container(
+                  height: 250,
+                  width: 250,
+                  decoration: const BoxDecoration(color: Colors.deepOrange),
+                ),
+              ),
+              BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 100, sigmaY: 100),
+                child: Container(
+                  decoration: const BoxDecoration(color: Colors.transparent),
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
