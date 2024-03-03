2de648bfe101a6d31d6f9a9bdfbdaee1f9ee8280
shivam_since2002 <sg2109964@gmail.com>
2024-03-03 07:12:09 +0530

    sab sahi chal rha 2

--- lib/Services/ColorGameScreen/src/screen/ColorGameScreen.dart
+++ lib/Services/ColorGameScreen/src/screen/ColorGameScreen.dart
@@ -1,16 +1,174 @@
 import 'package:flutter/material.dart';
+import 'package:strive/Services/common/src/utils/MediaQuary.dart';
 
 class ColorGame extends StatelessWidget {
   const ColorGame({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          "Start Game",
-          style: TextStyle(fontSize: 30, color: Colors.white),
+    double screenHeight = ScreenSize.height(context);
+    double screenWidth = ScreenSize.width(context);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          //Container Starts form Here
+          child: Container(
+            height: screenHeight * 0.9,
+            width: screenWidth * 1,
+            decoration: const BoxDecoration(color: Colors.black),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                //Contianer for Color Options
+                Container(
+                  height: screenHeight * 0.27,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    color: Colors.white,
+                  ),
+                  //Inside  Container have a columne
+                  child: Column(
+                    children: <Widget>[
+                      //Inside Column have a 3 Row
+                      //First Row for Text
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: EdgeInsets.all(8.0),
+                            child: Row(
+                              children: [
+                                Icon(Icons.emoji_events_outlined),
+                                Text(
+                                  " Parity",
+                                  style: TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                      const Divider(
+                        indent: BorderSide.strokeAlignCenter,
+                        color: Colors.black,
+                      ),
+                      //Second Row for Color Container
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: <Widget>[
+                          //Red Container
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    30,
+                                  ),
+                                ),
+                                color: Colors.red),
+                            child: const Center(
+                              child: CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: AssetImage(
+                                  'assets/images/rocket.png',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    30,
+                                  ),
+                                ),
+                                color: Colors.deepPurple),
+                            child: const Center(
+                              child: CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: AssetImage(
+                                  'assets/images/rocket.png',
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(
+                                    30,
+                                  ),
+                                ),
+                                color: Colors.green),
+                            child: const Center(
+                              child: CircleAvatar(
+                                backgroundColor: Colors.transparent,
+                                backgroundImage: AssetImage(
+                                  'assets/images/rocket.png',
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                      //Third Row button Taking action
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: <Widget>[
+                            SizedBox(
+                              width: 100,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Red",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Violet",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 100,
+                              child: ElevatedButton(
+                                onPressed: () {},
+                                child: const Text(
+                                  "Green",
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
         ),
       ),
     );
